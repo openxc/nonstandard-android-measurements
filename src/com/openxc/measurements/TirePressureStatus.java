5bdce642e9b186563e9e57fb13e1252724eca578
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-08-30 10:39:52 -0400

    Combine 4 tire pressure status meas. into one evented measurement.

---
+++ src/com/openxc/measurements/TirePressureStatus.java
@@ -0,0 +1,44 @@
+package com.openxc.measurements;
+
+import java.util.Locale;
+
+import com.openxc.measurements.TirePressure.Tire;
+import com.openxc.units.PoundsPerSquareInch;
+import com.openxc.units.State;
+
+public class TirePressureStatus extends BaseMeasurement<State<TirePressure.Tire>> {
+    public final static String ID = "tire_pressure_status";
+
+    public enum PressureStatus {
+        LOW,
+        NORMAL,
+        HIGH
+    }
+
+    public TirePressureStatus(State<Tire> tire, State<PressureStatus> pressure) {
+        super(tire, pressure);
+    }
+
+    public TirePressureStatus(Tire tire, PressureStatus pressure) {
+        this(new State<Tire>(tire), new State<PressureStatus>(pressure));
+    }
+
+    public TirePressureStatus(Tire tire, String status) {
+        this(tire, PressureStatus.valueOf(status.toUpperCase(Locale.US)));
+    }
+
+    @Override
+    public String getSerializedValue() {
+        return getValue().enumValue().toString().toLowerCase();
+    }
+
+    @Override
+    public PoundsPerSquareInch getEvent() {
+        return (PoundsPerSquareInch) super.getEvent();
+    }
+
+    @Override
+    public String getGenericName() {
+        return ID;
+    }
+}
