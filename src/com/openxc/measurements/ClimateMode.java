516be8d4230924052b57a954538a034cbd947004
U-FORDNA1\jkristin <jkristin@AEC1W7BDSQ2Q1.aec.ford.com>
2013-09-07 03:07:47 -0400

    Changed climate_control to new definition

--- src/com/openxc/measurements/ClimateMode.java
+++ src/com/openxc/measurements/ClimateMode.java
@@ -11,18 +11,18 @@ public class ClimateMode extends BaseMeasurement<State<ClimateMode.ClimateContro
 	public final static String ID = "climate_mode";
 	
 	public enum ClimateControls {
-		OFF,
-		MAX_AC,
-		PANEL_VENT,
-		FLOOR,
-		WINDSHIELD_FLOOR,
-		WINDSHIELD_DEFROST,
-		WINDSHIELD_PANEL,
-		WINDSHIELD_PANEL_FLOOR,
-		MAX_DEFROST,
-		AUTO,
-		LAST_USER_SETTING,
-		NOT_USED
+		UNUSED(0),
+		PANEL_VENT(1),
+		PANEL_FLOOR(2),
+		FLOOR(3),
+		FAN_SPEED_INCREMENT(11),
+		FAN_SPEED_DECREMENT(12),
+		AUTO(13),
+		MAX_AC(14),
+		RECIRCULATION(15),
+		FRONT_DEFROST(16),
+		REAR_DEFROST(17),
+		MAX_DEFROTS(22)
 	}
 
 	public ClimateMode(State<ClimateControls> value) {
